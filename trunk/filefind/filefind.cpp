7d09096debcea33d53a32830dbd51d96706f21d2
shuaimingdong@gmail.com <shuaimingdong@gmail.com@a1eeebb8-d561-62e9-21b9-13109e616c5b>
2012-07-20 13:44:41 +0000

    insertsort

--- trunk/filefind/filefind.cpp
+++ trunk/filefind/filefind.cpp
@@ -2,17 +2,34 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
 using namespace std;
-void listDir(char *path)
+void swap(int &a,int &b)
+{
+  a=a^b;
+  b=a^b;
+  a=a^b;
+}
+void listDir(int *a,int n)
 {
 	
-    
+    for (int i=0;i<n-1;i++)
+    {
+		for (int j=i+1;j>0&&a[j]<a[j-1];j--)
+		{
+			swap(a[j],a[j-1]);
+		}
+    }
+	//return a;
 }
 
 int main(int argc,char *argv[])
 {
-
-	listDir("D://amit");
-	return 0;
+	int  a[]={5,3,6,7,1,2,8,4};
+	listDir(a,8);
+	for (int i=0;i<8;i++)
+	{
+		cout<<a[i]<<ends;
+	}
+	
 }
